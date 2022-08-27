f3db7abf964474b36553bf703f9dc12b16f9d529
TomiSaputraa <tommycodingjj@gmail.com>
2022-08-27 12:15:34 +0700

    making nested json

--- app/Http/Controllers/ProductsApiController.php
+++ app/Http/Controllers/ProductsApiController.php
@@ -7,29 +7,41 @@ use Illuminate\Http\Request;
 
 class ProductsApiController extends Controller
 {
-    public function index() {
+    public function index()
+    {
+        // making nested api
         $products = Product::all();
-        return response()->json(['message' => 'success','data' => $products]);
+        $username = Product::select('products.name as name', 'products.id as id')->get();
+        return response()->json([
+            'message' => '200 OK',
+            'product' => $products,
+            "username" => $username,
+
+        ]);
     }
 
-    public function show($id) {
+    public function show($id)
+    {
         $product = Product::find($id);
-        return response()->json(['message' => 'success','data' => $product]);
+        return response()->json(['message' => 'success', 'data' => $product]);
     }
 
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
         $product = Product::create($request->all());
-        return response()->json(['message' => 'insert data has been inserted success','data' => $product]);
+        return response()->json(['message' => 'insert data has been inserted success', 'data' => $product]);
     }
 
-    public function update(Request $request, $id) {
+    public function update(Request $request, $id)
+    {
         $product = Product::find($id);
         $product->update($request->all());
-        return response()->json(['message' => 'success update','data' => $product]);
+        return response()->json(['message' => 'success update', 'data' => $product]);
     }
-    public function destroy($id) {
+    public function destroy($id)
+    {
         $product = Product::find($id);
         $product->delete();
-        return response()->json(['message' => 'success delete','data' => null]);
+        return response()->json(['message' => 'success delete', 'data' => null]);
     }
 }
